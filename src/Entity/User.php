edf6d433376d599c8779fea5a7abb00cb63020df
Zachary Zhang <9669105@qq.com>
2019-08-29 21:14:47 +0800

    example

---
+++ src/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * User
+ *
+ * @author   Zachary Zhang <9669105@qq.com>
+ * @package  App\Entity
+ */
+class User
+{
+    protected $id;
+    protected $name;
+}
